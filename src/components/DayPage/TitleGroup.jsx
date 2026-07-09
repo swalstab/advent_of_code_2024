3e44dcbbbeb656395f3d5139ee18f8aeed6d07fa
StfnVlds <stefanie.walstab@gmail.com>
2026-07-09 16:18:34 +0200

    add component TitleGroup

---
+++ src/components/DayPage/TitleGroup.jsx
@@ -0,0 +1,30 @@
+import { useParams } from "react-router";
+import IconLink from "./IconLink";
+
+function TitleGroup() {
+  const { day } = useParams();
+
+  return (
+    <div className="header__title-group">
+      {Number(day) > 1 && (
+        <IconLink
+          to={`/day/${Number(day) - 1}`}
+          ariaLabel="Go to previous solved day"
+          icon="left"
+        />
+      )}
+      <h1 className="heading--xl" style={{ gridColumn: 2 }}>
+        Day {day}
+      </h1>
+      {Number(day) < 25 && (
+        <IconLink
+          to={`/day/${Number(day) + 1}`}
+          ariaLabel="Go to next solved day"
+          icon="right"
+        />
+      )}
+    </div>
+  );
+}
+
+export default TitleGroup;
