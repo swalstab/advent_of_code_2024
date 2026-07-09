779f1a7e8f4ace9bceb2a3a44dc5bb9505cf8ab1
StfnVlds <stefanie.walstab@gmail.com>
2026-07-09 16:17:09 +0200

    add component HeaderAction

---
+++ src/components/DayPage/HeaderAction.jsx
@@ -0,0 +1,14 @@
+function HeaderAction() {
+  return (
+    <a
+      className="header__action"
+      href=""
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Explore Task &rarr;
+    </a>
+  );
+}
+
+export default HeaderAction;
